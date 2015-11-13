ed1cfa8db9855c7c7fb3a41b96e55c7371ef8008
Eugene Guzik <guzik.evgeny@gmail.com>
2015-11-13 19:19:04 +0200

    finished

--- app/src/main/java/ua/com/elius/eugene/myappportfolio/MainActivity.java
+++ app/src/main/java/ua/com/elius/eugene/myappportfolio/MainActivity.java
@@ -1,7 +1,10 @@
 package ua.com.elius.eugene.myappportfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.button1_toast), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.button2_toast), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.button3_toast), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.button4_toast), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        Button button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.button5_toast), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        Button button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.button6_toast), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
 }
